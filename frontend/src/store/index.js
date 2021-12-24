82a0312f26d54f71fd41f1ec3734b06de29bc7ee
AliNamooya <ali.namooya1@gmail.com>
2021-12-24 21:27:31 +0100

    frontend: SignUp OK
    On va maintenant rajouter le logIn

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,12 +1,29 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+const axios = require("axios");
 
-export default createStore({
-  state: {
-  },
-  mutations: {
-  },
+const instance = axios.create({
+  baseURL: "http://localhost:3000/api/user",
+});
+
+const store = createStore({
+  state: {},
+  mutations: {},
   actions: {
+    createAccount: ({ commit }, userInfos) => {
+      return new Promise((resolve, reject) => {
+        commit;
+        instance
+          .post("/signup", userInfos)
+          .then(function (response) {
+            resolve(response);
+          })
+          .catch(function (error) {
+            reject(error);
+          });
+      });
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
+
+export default store;
